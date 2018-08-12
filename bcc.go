f615a0be4080317c1b9456b2d2cbcfcaeef0a3ae
soyoo <soyoo@aliyun.com>
2018-08-12 11:32:57 +0800

    Update bcc.go

--- bcc.go
+++ bcc.go
@@ -1,1 +1,14 @@
+// bcc verification algorithm implementation.
+package check
 
+import "fmt"
+import "byte"
+
+func Bcc(data []byte) uint8 {
+  var (
+    bcc uint8
+    length int = len(data)
+    index int
+  )
+  
+}
