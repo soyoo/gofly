d4b8db66422de3ff945baeb467fd9d9e5f82a1ec
soyoo <soyoo@aliyun.com>
2018-08-14 18:24:05 +0800

    Update bcc.go

--- check/bcc.go
+++ check/bcc.go
@@ -1,4 +1,9 @@
 // bcc verification algorithm implementation.
+//     BCC(Block Check Character/信息组校验码)，因校验码是将所有数据异或得出，故俗称异或校验。
+// 具体算法是：将每一个字节的数据（一般是两个16进制的字符）进行异或后即得到校验码。 
+// 例如16进制数据：01 A0 7C FF 02 
+// 计算：01 xor A0 xor 7C xor FF xor 02 = 20 
+// 校验码是：20 
 package check
 
 func Bcc(data []byte) uint8 {
